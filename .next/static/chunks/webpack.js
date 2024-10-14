c1745fd2e6da58b9372004b65b534ee8958d58fb
oykudemir <oykuudemirr16@gmail.com>
2024-10-14 14:31:57 +0300

    VSO-15 :: landing page initial implementation

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "24d67d154785ee5f"; }
+/******/ 		__webpack_require__.h = function() { return "acc5e1dc90b81b79"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
